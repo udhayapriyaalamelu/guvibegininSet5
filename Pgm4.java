83a738b64b036f64bce1f658fea44e7a74660825
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-07-27 11:40:56 +0530

    Add files via upload

---
+++ Pgm4.java
@@ -0,0 +1,15 @@
+package guvi_set5;
+import java.util.*;
+public class Pgm4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int range=sc.nextInt();
+if(range>=1&&range<=10)
+	System.out.println("Yes");
+else
+	System.out.println("No");
+	}
+
+}
