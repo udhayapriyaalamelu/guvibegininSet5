ea9c15459e06e3fbc8d69ad46dd5c619387d7950
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-07-27 14:15:35 +0530

    Add files via upload

---
+++ Pgm15.java
@@ -0,0 +1,17 @@
+package guvi_set5;
+import java.util.*;
+public class Pgm15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int m=sc.nextInt();
+int pod=n*m;
+if(pod%2==0)
+	System.out.println("even");
+else
+	System.out.println("Odd");
+	}
+
+}
