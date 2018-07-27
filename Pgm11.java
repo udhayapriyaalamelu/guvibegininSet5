67e244444a0a54e7e06ff81ee25f2a1c5d6f7dab
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-07-27 13:48:38 +0530

    Add files via upload

---
+++ Pgm11.java
@@ -0,0 +1,17 @@
+package guvi_set5;
+import java.util.*;
+public class Pgm11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+String str=Integer.toString(num);
+char ch[]=str.toCharArray();
+for(int i=0;i<ch.length;i++)
+{
+	System.out.print(ch[i]+" ");
+}
+	}
+
+}
