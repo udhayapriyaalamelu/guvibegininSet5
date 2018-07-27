ff3ca6bddf9aac90b43ffe81e777972aeb2eb48e
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-07-27 13:37:25 +0530

    Add files via upload

---
+++ Pgm10.java
@@ -0,0 +1,14 @@
+package guvi_set5;
+import java.util.*;
+public class Pgm10 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n%2==0)
+	System.out.println("Yes");
+else
+	System.out.println("No");
+	}
+
+}
