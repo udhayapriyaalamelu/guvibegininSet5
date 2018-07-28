e34af338ac5c2b0cb597dc3f1789824cba6f80e0
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-07-28 15:15:06 +0530

    Add files via upload

---
+++ Pgm21.java
@@ -0,0 +1,13 @@
+package guvi_set5;
+import java.util.*;
+public class Pgm21 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+String str=sc.nextLine();
+int k=sc.nextInt();
+	System.out.println(str.substring(0, k));
+	}
+
+}
