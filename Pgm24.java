1fa19ab26464b1733494a79fd2e265a40801a9eb
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-07-28 16:22:23 +0530

    Add files via upload

---
+++ Pgm24.java
@@ -0,0 +1,17 @@
+package guvi_set5;
+import java.util.*;
+public class Pgm24 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int m=sc.nextInt();
+int a=n+m;
+if(a%2==0)
+	System.out.println("even");
+else
+	System.out.println("odd");
+	}
+
+}
