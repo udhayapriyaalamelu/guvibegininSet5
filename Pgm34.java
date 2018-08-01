be85960aaff87d820e4e820ccb575c9815312292
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-08-01 11:47:57 +0530

    Add files via upload

---
+++ Pgm34.java
@@ -0,0 +1,12 @@
+package guvi_set5;
+import java.util.*;
+public class Pgm34 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+float f=sc.nextFloat();
+System.out.println((int)Math.ceil(f));
+	}
+
+}
