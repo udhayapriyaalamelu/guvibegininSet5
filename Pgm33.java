d3916b2dae8b544639d8992b8adf6272ed25a062
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-08-01 11:32:05 +0530

    Add files via upload

---
+++ Pgm33.java
@@ -0,0 +1,17 @@
+package guvi_set5;
+import java.util.*;
+public class Pgm33 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int l=sc.nextInt();
+int r=sc.nextInt();
+int n=sc.nextInt();
+if((n>l)&&(n<r))
+	System.out.println("yes");
+else
+	System.out.println("No");
+	}
+
+}
